9402f38221a2decd9e5e0156a4b003d5b6ce4641
petar puharić <puharicpetar103@gmail.com>
2025-10-21 20:47:12 +0200

    Prvi commit

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Proizvod proizvod = new Proizvod("Laptop", 1500.00m, 10);
+            Proizvod drugiProizvod = new Proizvod("Pametni telefon", 800.00m, 25);
+            proizvod.IspisiInformacije();
+            drugiProizvod.IspisiInformacije();
+
+
+            PametniTelefon telefon = new PametniTelefon("iPhone 13", "Apple", 2021, "iOS");
+            telefon.InstalirajAplikaciju("WhatsApp");
         }
     }
 }
---
+++ ConsoleApp/Proizvod.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Proizvod
+    {
+        public string Naziv { get; set; }
+        public decimal Cijena { get; set; }
+        public int DostupnaKolicina { get; set; }
+
+        public Proizvod(string naziv, decimal cijena, int dostupnaKolicina) //Konstruktor
+        {
+            Naziv = naziv;
+            Cijena = cijena;
+            DostupnaKolicina = dostupnaKolicina;
+        }
+
+        public void IspisiInformacije()
+        {
+            Console.WriteLine($"Proizvod: {Naziv}, Cijena: {Cijena}, Dostupna Količina: {DostupnaKolicina}");
+        }
+
+
+    }
+
+    public class ElektronickiUredjaj
+    {
+        public string Naziv { get; set; }
+        public string Proizvodjac { get; set; }
+        public int GodinaProizvodnje { get; set; }
+
+        public ElektronickiUredjaj(string naziv, string proizvodjac, int godinaProizvodnje)
+        {
+            Naziv = naziv;
+            Proizvodjac = proizvodjac;
+            GodinaProizvodnje = godinaProizvodnje;
+        }
+    }
+    public class PametniTelefon : ElektronickiUredjaj
+    {
+        public string OperativniSustav { get; set; }
+        public PametniTelefon(string naziv, string proizvodjac, int godinaProizvodnje, string operativniSustav)
+            : base(naziv, proizvodjac, godinaProizvodnje)
+        {
+            OperativniSustav = operativniSustav;
+        }
+        public void InstalirajAplikaciju(string nazivAplikacije)
+        {
+            Console.WriteLine($"Instalirana aplikacija: {nazivAplikacije} na {Naziv}");
+        }
+
+    }
+
+}
