6ce3a06ceadf29d6cd13cf5f091c657053cfe76f
petar puharić <puharicpetar103@gmail.com>
2025-10-21 20:50:54 +0200

    .

---
+++ ConsoleApp/Elektronik.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp
+{
+    public class ElektronickiUredjaj
+    {
+        public string Naziv { get; set; }
+        public string Proizvodjac { get; set; }
+        public int GodinaProizvodnje { get; set; }
+
+        public ElektronickiUredjaj(string naziv, string proizvodjac, int godinaProizvodnje)
+        {
+            Naziv = naziv;
+            Proizvodjac = proizvodjac;
+            GodinaProizvodnje = godinaProizvodnje;
+        }
+    }
+    public class PametniTelefon : ElektronickiUredjaj
+    {
+        public string OperativniSustav { get; set; }
+        public PametniTelefon(string naziv, string proizvodjac, int godinaProizvodnje, string operativniSustav)
+            : base(naziv, proizvodjac, godinaProizvodnje)
+        {
+            OperativniSustav = operativniSustav;
+        }
+        public void InstalirajAplikaciju(string nazivAplikacije)
+        {
+            Console.WriteLine($"Instalirana aplikacija: {nazivAplikacije} na {Naziv}");
+        }
+
+    }
+}
--- ConsoleApp/Proizvod.cs
+++ ConsoleApp/Proizvod.cs
@@ -17,36 +17,5 @@
         {
             Console.WriteLine($"Proizvod: {Naziv}, Cijena: {Cijena}, Dostupna Količina: {DostupnaKolicina}");
         }
-
-
     }
-
-    public class ElektronickiUredjaj
-    {
-        public string Naziv { get; set; }
-        public string Proizvodjac { get; set; }
-        public int GodinaProizvodnje { get; set; }
-
-        public ElektronickiUredjaj(string naziv, string proizvodjac, int godinaProizvodnje)
-        {
-            Naziv = naziv;
-            Proizvodjac = proizvodjac;
-            GodinaProizvodnje = godinaProizvodnje;
-        }
-    }
-    public class PametniTelefon : ElektronickiUredjaj
-    {
-        public string OperativniSustav { get; set; }
-        public PametniTelefon(string naziv, string proizvodjac, int godinaProizvodnje, string operativniSustav)
-            : base(naziv, proizvodjac, godinaProizvodnje)
-        {
-            OperativniSustav = operativniSustav;
-        }
-        public void InstalirajAplikaciju(string nazivAplikacije)
-        {
-            Console.WriteLine($"Instalirana aplikacija: {nazivAplikacije} na {Naziv}");
-        }
-
-    }
-
 }
